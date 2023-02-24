87c3c280ede2138aedd80e5e8700c02733c02124
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-02-23 20:35:42 -0500

    added signInRequest field
    +added web_client_id string resource value

--- app/src/main/java/com/locoquest/app/MainActivity.kt
+++ app/src/main/java/com/locoquest/app/MainActivity.kt
@@ -2,8 +2,21 @@ package com.locoquest.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
 
 class MainActivity : AppCompatActivity() {
+
+    val signInRequest = BeginSignInRequest.builder()
+        .setGoogleIdTokenRequestOptions(
+            BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                .setSupported(true)
+                // Your server's client ID, not your Android client ID.
+                .setServerClientId(getString(R.string.web_client_id))
+                // Only show accounts previously used to sign in.
+                .setFilterByAuthorizedAccounts(true)
+                .build())
+        .build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
