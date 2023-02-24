af2510f4bdb2e2c38796ef4f9dc992c5eff0baa9
Derrick Adkins <derrick.l.adkins@gmail.com>
2023-02-23 20:07:51 -0500

    Update RetrofitClientInstance.kt
    + update name attribute of activity in manifest

--- app/src/main/java/com/locoquest/app/RetrofitClientInstance.kt
+++ app/src/main/java/com/locoquest/app/RetrofitClientInstance.kt
@@ -1,4 +1,22 @@
 package com.locoquest.app
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetrofitClientInstance {
+    private var retrofit: Retrofit? = null;
+    private val BASE_URL = ""//TODO
+
+    val retrofitInstance : Retrofit?
+        get() {
+            // has this object been created yet?
+            if (retrofit == null) {
+                // create it!
+                retrofit = retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return retrofit
+        }
 }
