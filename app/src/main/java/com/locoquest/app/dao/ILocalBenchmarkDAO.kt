49d8afef956f716c9100e980af8b120732ec7bf0
bohmanwl <112824679+bohmanwl@users.noreply.github.com>
2023-02-26 19:20:19 -0500

    Addition of Benchmark and LocationDetails DAO

---
+++ app/src/main/java/com/locoquest/app/dao/ILocalBenchmarkDAO.kt
@@ -0,0 +1,18 @@
+package com.locoquest.app.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.locoquest.app.dto.Benchmark
+
+
+@Dao
+interface ILocalBenchmarkDAO {
+    @Query("SELECT * FROM benchmark")
+    fun getBenchmark() : LiveData<List<Benchmark>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(benchmark: ArrayList<Benchmark>)
+
+    @Delete
+    fun delete(benchmark: Benchmark)
+}
