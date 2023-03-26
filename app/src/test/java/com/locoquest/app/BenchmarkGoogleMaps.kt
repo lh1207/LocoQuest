44584915f1b270bbd0e9b5528f6f33429ff0cc64
bohmanwl <112824679+bohmanwl@users.noreply.github.com>
2023-03-26 15:31:32 -0400

    Added a separate Kotlin file for Google maps benchmarks tests

---
+++ app/src/test/java/com/locoquest/app/BenchmarkGoogleMaps.kt
@@ -0,0 +1,11 @@
+package com.locoquest.app
+
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+
