d7afe2c634d5e4b27f52f01411c9c405d411d84a
bohmanwl <112824679+bohmanwl@users.noreply.github.com>
2023-03-26 15:43:30 -0400

    created an @before function for Google maps unit tests so that a mock map will be generated before each unitTest

--- app/src/test/java/com/locoquest/app/BenchmarkGoogleMaps.kt
+++ app/src/test/java/com/locoquest/app/BenchmarkGoogleMaps.kt
@@ -2,6 +2,7 @@ package com.locoquest.app
 
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -9,3 +10,19 @@ import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
 
+class BenchmarkMapGeneration {
+    @Mock
+    private lateinit var mapView: MapView
+
+    @Mock
+    private lateinit var googleMap: GoogleMap
+
+    @Before
+    fun mapGenerate() {
+        MockitoAnnotations.initMocks(this)
+        `when`(mapView.getMapAsync(any())).then {
+            val callback = it.arguments[0] as OnMapReadyCallback
+            callback.onMapReady(googleMap)
+        }
+    }
+}
