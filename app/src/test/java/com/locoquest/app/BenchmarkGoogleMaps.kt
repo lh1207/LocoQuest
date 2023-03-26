7c4150c7845007d7b97846a5bd52d673bd29fb22
bohmanwl <112824679+bohmanwl@users.noreply.github.com>
2023-03-26 16:38:36 -0400

    Added comments to explain Map unit tests

--- app/src/test/java/com/locoquest/app/BenchmarkGoogleMaps.kt
+++ app/src/test/java/com/locoquest/app/BenchmarkGoogleMaps.kt
@@ -10,7 +10,14 @@ import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
 
+/*
+    - These unit tests ensure the application can properly generate Google Maps
+    - mokito is used to generate a temporary "map" for the tests, so an actual map wont need to be generated
+*/
+
 class MapsTest {
+    // Generate a mock version of google maps, for the purpose of unit tests.
+    // @Before will be run before each unit test, this is what creates a temporary map instance
     @Mock
     private lateinit var mapView: MapView
 
@@ -26,10 +33,15 @@ class MapsTest {
         }
     }
 
+    // Tests that a google map instance is generated. Test will fail if returned Null, meaning
+    // no map was generated.
     @Test
     fun testMapNotNull() {
         assertNotNull(mapView.getMapAsync { })
     }
+
+    // Below is a mock unit test for adding markers, currented commented out as it was causing
+    // compilation errors, and is not needed in current build
 
     /*@Test
     fun testMarkerAdded() {
