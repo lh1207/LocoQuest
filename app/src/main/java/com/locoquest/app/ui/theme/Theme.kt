dc9560fce8fb52c52a3932081861485d2ff930b0
francescasuba <subaap@mail.uc.edu>
2023-03-28 13:26:36 -0400

    Updated theme method name

--- app/src/main/java/com/locoquest/app/ui/theme/Theme.kt
+++ app/src/main/java/com/locoquest/app/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun IndividualAssignment304832Theme(
+fun locoQuestTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
