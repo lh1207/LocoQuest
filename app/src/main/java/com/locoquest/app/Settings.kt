0f07d16dcfcfd54415aec6fdf4a203fdda9afd50
Levi H <levihuff1207@gmail.com>
2023-04-19 17:51:20 -0400

    initalized settings.kt

--- app/src/main/java/com/locoquest/app/Settings.kt
+++ app/src/main/java/com/locoquest/app/Settings.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Switch
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,36 @@ class Settings : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Get references to the switches and button in the layout
+        val notificationToggle = view.findViewById<Switch>(R.id.notificationToggle)
+        val preciseLocationToggle = view.findViewById<Switch>(R.id.preciseLocationToggle)
+        val approximateLocationToggle = view.findViewById<Switch>(R.id.approximateLocationToggle)
+        val requestPermissionsButton = view.findViewById<Button>(R.id.requestPermissionsButton)
+
+        // Set an event listener for the notification toggle switch
+        notificationToggle.setOnCheckedChangeListener { _, _ ->
+            // Do something when the toggle is checked or unchecked
+        }
+
+        // Set an event listener for the precise location toggle switch
+        preciseLocationToggle.setOnCheckedChangeListener { _, _ ->
+            // Do something when the toggle is checked or unchecked
+        }
+
+        // Set an event listener for the approximate location toggle switch
+        approximateLocationToggle.setOnCheckedChangeListener { _, _ ->
+            // Do something when the toggle is checked or unchecked
+        }
+
+        // Set an event listener for the request permissions button
+        requestPermissionsButton.setOnClickListener {
+            // Do something when the button is clicked
+        }
     }
 
     companion object {
